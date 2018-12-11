58d3030f4c7dfc0b95e3d35bc933954f373fc10c
barnabaskim95 <barnabaskim95@gmail.com>
2018-12-11 16:20:55 -0500

    Retrieves all href and script tags on checkout pg

---
+++ bs_example.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+
+URL="https://hackathon.wopr.cc/index.php/checkout/#payment"
+r= requests.get(URL)
+html_content=r.text
+soup=BeautifulSoup(html_content,"html.parser")
+
+soup.findAll('script') #Returns array of all <script> tags
+href_tags= soup.find_all(href=True) #Array containing href tags
+script_tags= soup.find_all('script')# Array containing all scripts
+
