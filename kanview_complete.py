af25f441745b62b2f2e3849da492a697ac12e5b0
WheelsVT <WheelsVT@users.noreply.github.com>
2018-12-11 16:58:57 -0500

    Add files via upload
    Item adds, moves to shipping, adds a fake email address, requests payment page, selects VISA.
    Should be set to parse any javascript on the page.

--- kanview_complete.py
+++ kanview_complete.py
@@ -9,25 +9,27 @@ from tabulate import tabulate
 import os
 import time
 
+
 #launch url
 url = "https://hackathon.wopr.cc/index.php/didi-sport-watch.html"
-buttonone = "action more button"
+urlcheckout = "https://hackathon.wopr.cc/index.php/checkout/"
+urlpayment = "https://hackathon.wopr.cc/index.php/checkout/#payment"
 
 # create a new Firefox session
-#driver = webdriver.Firefox()
-#profile = webdriver.FirefoxProfile()
-#profile.set_preference("browser.cache.disk.enable", False)
-#profile.set_preference("browser.cache.memory.enable", False)
-#profile.set_preference("browser.cache.offline.enable", False)
-#profile.set_preference("network.http.use-cache", False)
-#driver = webdriver.Firefox(profile)
+driver = webdriver.Firefox()
+profile = webdriver.FirefoxProfile()
+profile.set_preference("browser.cache.disk.enable", False)
+profile.set_preference("browser.cache.memory.enable", False)
+profile.set_preference("browser.cache.offline.enable", False)
+profile.set_preference("network.http.use-cache", False)
+driver = webdriver.Firefox(profile)
 
 #create Chrome session
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'  
+#chrome_options = Options()  
+#chrome_options.add_argument("--headless")  
+#chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'  
 #driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"), chrome_options=chrome_options)
-driver = webdriver.Chrome("chromedriver")
+#driver = webdriver.Chrome("chromedriver")
 
 driver.get(url)
 print "Waiting for page to completely load..."
@@ -53,16 +55,37 @@ productpage = soup_level1.get_text()
 print "Preparing to add item to cart..."
 python_button = driver.find_element_by_id("product-addtocart-button")
 python_button.click()
-print("product added. Looking for cart.")
+print("Product added. Looking for cart.")
 time.sleep(5)
 
 #click the cart button to view the cart
 #python_button = driver.find_element_by_partial_link_text('checkout')
-python_button = driver.find_element_by_xpath("action showcart")
-python_button.click()
-print("Viewing Cart")
+#python_button = driver.find_element_by_xpath("//a[contains(@class, 'checkout')]")
+#python_button = driver.find_element_by_css_selector("minicart-wrapper")
+#python_button.click()
+#print("Moving to Checkout")
 
 #click the checkout button...
+driver.get(urlcheckout)
+time.sleep(5)
+
+#fill in shipping details
+#driver.find_element_by_xpath("//*[@id='customer-email']").SendKeys("testadfadjre@mailinator.com")
+driver.find_element_by_id("customer-email").send_keys("testadfadfadfad@mailinator.com")
+
+
+#Click Next
+#or lets just skip it and go right to the payment page.
+driver.get(urlpayment)
+time.sleep(5)
+
+#Select Visa...
+driver.find_element_by_id("braintree").click()
+time.sleep(2)
+
+#NOW we're on the payment page! Scrape for all javascript here
+print "Payment page loaded. Scraping for javascript elements."
+
 
 
 #Selenium hands of the source of the specific job page to Beautiful Soup
