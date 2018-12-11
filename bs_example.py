c539a5d7803acd93e914804e55dfe1e9ed3b3d94
barnabaskim95 <barnabaskim95@gmail.com>
2018-12-11 16:22:40 -0500

    Retrieves all href and script tags on checkout pg
    Removed extra code

--- bs_example.py
+++ bs_example.py
@@ -7,7 +7,6 @@ r= requests.get(URL)
 html_content=r.text
 soup=BeautifulSoup(html_content,"html.parser")
 
-soup.findAll('script') #Returns array of all <script> tags
 href_tags= soup.find_all(href=True) #Array containing href tags
 script_tags= soup.find_all('script')# Array containing all scripts
 
